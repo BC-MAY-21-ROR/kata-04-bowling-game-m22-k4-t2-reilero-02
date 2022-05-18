23b7148bb2b067533b2b157f2159ec7a8463c94b
d1eg0dev <diego@studiowd.com.mx>
2022-05-18 17:38:55 -0500

    Refactor subjects of tests
    Co-authored-by: ruruisu <zaddozgl@gmail.com>
    Co-authored-by: lucasvidela94 <lucas.videla994@gmail.com>

--- app/game.rb
+++ app/game.rb
@@ -4,28 +4,29 @@ require_relative "./frame"
 class Game
     def initialize
         @shot
+        @score
     end
 
     def play_frame(frame)
-        frame_score = 0
-        puts frame.pins_dropped(rand(0..frame.remaining_pins))
+        @frame_score = 0
+        @frame_score += frame.pins_dropped(rand(0..frame.remaining_pins))
+        puts  @frame_score
         if frame.remaining_pins==0
             puts "strike"
-            return 
+            return @frame_score 
         end
-        puts frame.pins_dropped(rand(0..frame.remaining_pins))
+        @frame_score += frame.pins_dropped(rand(0..frame.remaining_pins))
+        puts  @frame_score
         if frame.remaining_pins==0
             puts "spare"
         end
+        @frame_score
+        puts @frame_score
     end
 
     def past_shot(frame)
         @index = board.find_index(frame)
         @board[index - 1]
-    end
-
-    def final_score
-        @score = 0
     end
 
 end
